5089af0fb0fb37ef293e2886a96cc8014496c9ff
Ingmar Steen <iksteen@gmail.com>
2016-01-12 12:25:07 +0100

    Move MAKE_STD_VAL up in xlate php

--- translate_php_value.c
+++ translate_php_value.c
@@ -10,23 +10,19 @@ pyhp_translate_php_value(PyObject *value) {
     Py_ssize_t pos = 0;
     PyObject *key, *ivalue;
 
+    MAKE_STD_ZVAL(var);
+
     if (value == Py_None) {
-        MAKE_STD_ZVAL(var);
         ZVAL_NULL(var);
     } else if (PyBool_Check(value)) {
-        MAKE_STD_ZVAL(var);
         ZVAL_BOOL(var, (value == Py_True) ? 1 : 0);
     } else if (PyFloat_Check(value)) {
-        MAKE_STD_ZVAL(var);
         ZVAL_DOUBLE(var, PyFloat_AsDouble(value));
     } else if (PyInt_Check(value)) {
-        MAKE_STD_ZVAL(var);
         ZVAL_LONG(var, PyInt_AsLong(value));
     } else if (PyString_Check(value)) {
-        MAKE_STD_ZVAL(var);
         ZVAL_STRINGL(var, PyString_AsString(value), PyString_Size(value), 1);
     } else if (PyDict_Check(value)) {
-        MAKE_STD_ZVAL(var);
         array_init(var);
 
         while (PyDict_Next(value, &pos, &key, &ivalue)) {
@@ -45,7 +41,6 @@ pyhp_translate_php_value(PyObject *value) {
             add_assoc_zval(var, PyString_AsString(key), var2);
         }
     } else if (PyTuple_Check(value)) {
-        MAKE_STD_ZVAL(var);
         array_init(var);
 
         for (pos=0; pos < PyTuple_GET_SIZE(value); ++pos) {
@@ -59,7 +54,6 @@ pyhp_translate_php_value(PyObject *value) {
             add_next_index_zval(var, var2);
         }
     } else if (PyList_Check(value)) {
-        MAKE_STD_ZVAL(var);
         array_init(var);
 
         for (pos=0; pos < PyList_GET_SIZE(value); ++pos) {
@@ -73,9 +67,9 @@ pyhp_translate_php_value(PyObject *value) {
             add_next_index_zval(var, var2);
         }
     } else if (PyCallable_Check(value)) {
-        MAKE_STD_ZVAL(var);
         pyhp_create_python_object_proxy(var, value);
     } else {
+        zval_ptr_dtor(&var);
         PyErr_SetString(PyExc_ValueError, "Unsupported value type");
         var = NULL;
     }
