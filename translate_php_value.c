a20e3dc716fe5e846b403d482ba348efec8174d2
Ingmar Steen <iksteen@gmail.com>
2016-01-22 16:45:47 +0100

    Allow passing arrays from php to python.
    A heuristic is applied to determine whether to use a list or a
    dict: if all keys in the php arrays are integers and they are
    sequential starting at 0, a list of the values is passed to
    python. Otherwise a dict is passed.

--- translate_php_value.c
+++ translate_php_value.c
@@ -15,6 +15,61 @@ PyObject *pyhp_translate_php_value(zval *value) {
             return (Z_LVAL_P(value) == 0) ? Py_False : Py_True;
         case IS_STRING:
             return PyString_FromStringAndSize(Z_STRVAL_P(value), Z_STRLEN_P(value));
+        case IS_ARRAY: {
+            PyObject *ret_val_dict = PyDict_New(), *ret_val_arr = PyList_New(0);
+            zval **item;
+            HashTable *value_hash = Z_ARRVAL_P(value);
+            HashPosition value_pointer;
+            int n = 0;
+
+            for (zend_hash_internal_pointer_reset_ex(value_hash, &value_pointer);
+                 zend_hash_get_current_data_ex(value_hash, (void**)&item, &value_pointer) == SUCCESS;
+                 zend_hash_move_forward_ex(value_hash, &value_pointer)) {
+                PyObject *value = pyhp_translate_php_value(*item);
+                char *key;
+                uint key_len;
+                ulong index;
+
+                switch (zend_hash_get_current_key_ex(value_hash, &key, &key_len, &index, 0, &value_pointer)) {
+                    case HASH_KEY_IS_STRING: {
+                        PyObject *key_object = PyString_FromString(key);
+                        PyDict_SetItem(ret_val_dict, key_object, value);
+                        Py_DECREF(key_object);
+
+                        if (ret_val_arr) {
+                            Py_DECREF(ret_val_arr);
+                            ret_val_arr = NULL;
+                        }
+
+                        break;
+                    }
+                    case HASH_KEY_IS_LONG: {
+                        PyObject *key_object = PyLong_FromLong(index);
+                        PyDict_SetItem(ret_val_dict, key_object, value);
+                        Py_DECREF(key_object);
+
+                        if (ret_val_arr && index == n) {
+                            PyList_Append(ret_val_arr, value);
+                        } else if (ret_val_arr) {
+                            Py_DECREF(ret_val_arr);
+                            ret_val_arr = NULL;
+                        }
+
+                        break;
+                    }
+                }
+
+                Py_DECREF(value);
+                n++;
+            }
+
+            if (ret_val_arr) {
+                Py_DECREF(ret_val_dict);
+                return ret_val_arr;
+            }
+
+            return ret_val_dict;
+        }
         default:
             fprintf(stderr, "Unsupported type (%s)\n", zend_zval_type_name(value));
     }
